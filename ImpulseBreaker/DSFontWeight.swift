43c5b81b1011ac3094eed6a9002863af61c8f437
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2026-01-06 17:47:49 +0300

    Implement DSFontWeight

---
+++ ImpulseBreaker/DSFontWeight.swift
@@ -0,0 +1,15 @@
+//
+//  DSFontWeight.swift
+//  ImpulseBreaker
+//
+//  Created by Александр Павлицкий on 06.01.2026.
+//
+
+import SwiftUI
+
+// MARK: - DSFontWeight
+extension DSFont {
+    enum Weight {
+        static let tabIcon: Font.Weight = .semibold
+    }
+}
