d7dc386a4bc485947cf8e53e6ff7bcb792f0867a
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2026-01-06 15:14:44 +0300

    Initial commit with project

---
+++ ImpulseBreaker/ImpulseBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImpulseBreakerApp.swift
+//  ImpulseBreaker
+//
+//  Created by Александр Павлицкий on 06.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct ImpulseBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
